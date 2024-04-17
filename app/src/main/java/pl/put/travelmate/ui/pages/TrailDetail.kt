7c436e81feabb31e978c90707988a01e7bd5493b
Jakub Jadczak <jakubjadczak02@aoutlook.com>
2024-04-17 16:36:18 +0200

    Float action button dla telefonow

--- app/src/main/java/pl/put/travelmate/ui/pages/TrailDetail.kt
+++ app/src/main/java/pl/put/travelmate/ui/pages/TrailDetail.kt
@@ -1,8 +1,7 @@
 package pl.put.travelmate.ui.pages
 
-import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,15 +9,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
@@ -28,98 +27,122 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import pl.put.travelmate.data.Trail
 import pl.put.travelmate.ui.components.Stopwatch
+import androidx.compose.ui.Alignment
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TrailDetail(trail: Trail) {
     val context = LocalContext.current
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(16.dp)
-    ) {
-        Column(Modifier.fillMaxHeight(0.5f).verticalScroll(rememberScrollState())) {
-            Text(
-                text = trail.name,
-                style = MaterialTheme.typography.headlineMedium,
-                fontWeight = MaterialTheme.typography.headlineMedium.fontWeight,
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-            Text(
-                text = trail.description,
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-            Stopwatch(context)
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(
-                    text = "Distance: ${trail.distance ?: "Unknown"}",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                Text(
-                    text = "Elevation: ${trail.elevation ?: "Unknown"}",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(
-                    text = "Start: ${trail.startLocation ?: "Unknown"}",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                Text(
-                    text = "Stop: ${trail.stopLocation ?: "Unknown"}",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text(
-                    text = "Difficulty: ${trail.difficulty ?: "Unknown"}",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                Text(
-                    text = "Time: ${trail.time ?: "Unknown"}",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-            Text(
-                text = "Details: ${trail.longDescription ?: "Unknown"}",
-                style = MaterialTheme.typography.bodySmall
-            )
-        }
-        LazyColumn(
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(16.dp)
         ) {
-            items(trail.steps) { step ->
-                OutlinedCard(
+            Column(
+                Modifier
+                    .fillMaxHeight(0.5f)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Text(
+                    text = trail.name,
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = MaterialTheme.typography.headlineMedium.fontWeight,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Text(
+                    text = trail.description,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Stopwatch(context)
+                Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(8.dp)
+                        .padding(top = 8.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
                     Text(
-                        text = step.name,
-                        style = MaterialTheme.typography.headlineSmall,
-                        modifier = Modifier.padding(8.dp)
+                        text = "Distance: ${trail.distance ?: "Unknown"}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(bottom = 8.dp)
                     )
                     Text(
-                        text = "${step.name}, ${step.distance}, ${step.time}",
+                        text = "Elevation: ${trail.elevation ?: "Unknown"}",
                         style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.padding(8.dp)
+                        modifier = Modifier.padding(bottom = 8.dp)
                     )
                 }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(
+                        text = "Start: ${trail.startLocation ?: "Unknown"}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(bottom = 8.dp)
+                    )
+                    Text(
+                        text = "Stop: ${trail.stopLocation ?: "Unknown"}",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(
+                        text = "Difficulty: ${trail.difficulty ?: "Unknown"}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(bottom = 8.dp)
+                    )
+                    Text(
+                        text = "Time: ${trail.time ?: "Unknown"}",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+                Text(
+                    text = "Details: ${trail.longDescription ?: "Unknown"}",
+                    style = MaterialTheme.typography.bodySmall
+                )
             }
+            LazyColumn(
+                // LazyColumn content
+            ) {
+                items(trail.steps) { step ->
+                    OutlinedCard(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Text(
+                            text = step.name,
+                            style = MaterialTheme.typography.headlineSmall,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                        Text(
+                            text = "${step.name}, ${step.distance}, ${step.time}",
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
+                }
+            }
+        }
+        FloatingActionButton(
+            onClick = {
+                Toast.makeText(context, "Smile!", Toast.LENGTH_SHORT).show()
+            },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp),
+            containerColor = MaterialTheme.colorScheme.primary
+        ) {
+            Icon(
+                imageVector = Icons.Filled.CameraAlt,
+                contentDescription = "Camera"
+            )
         }
     }
 }
